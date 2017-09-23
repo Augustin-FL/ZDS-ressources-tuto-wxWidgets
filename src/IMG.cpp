5b07324898da70beae934af561bfb5d14289a056
Augustin-FL <Augustin-FL@users.noreply.github.com>
2017-09-23 12:01:45 +0200

    ajout des événements

--- src/IMG.cpp
+++ src/IMG.cpp
@@ -1,5 +1,4 @@
 #include <wx/wx.h>
-#include <wx/spinctrl.h>
 #include "IMG.h"
 
 Frame_IMG::Frame_IMG(): wxFrame(NULL, wxID_ANY, wxT("Calculateur d'IMG"), wxDefaultPosition, wxSize(800,500))
@@ -34,12 +33,12 @@ Frame_IMG::Frame_IMG(): wxFrame(NULL, wxID_ANY, wxT("Calculateur d'IMG"), wxDefa
 
 	/* 2 bis - Création des zones de résultats */
 
-	wxTextCtrl *input_resultat_IMG = new wxTextCtrl(fenetre, wxID_ANY, wxT("0.00 %"), wxDefaultPosition, wxDefaultSize, wxTE_READONLY|wxTE_CENTRE); // input servant au résultat de l'IMG
-	wxStaticText *texte_resultat_sain = new wxStaticText(fenetre, wxID_ANY, wxT("Vous avez une alimentation saine pour la santé."));// Phrase
+	input_resultat_IMG = new wxTextCtrl(fenetre, wxID_ANY, wxT("0.00 %"), wxDefaultPosition, wxDefaultSize, wxTE_READONLY|wxTE_CENTRE); // input servant au résultat de l'IMG
+	texte_resultat_sain = new wxStaticText(fenetre, wxID_ANY, wxT("Vous avez une alimentation saine pour la santé."));// Phrase
 
 	/* 3 - Création des éléments de saisie */
 
-	wxButton *bouton_raz = new wxButton(fenetre, wxID_ANY, wxT("Remise à zéro")); // bouton remise à zéro
+	bouton_raz = new wxButton(fenetre, wxID_ANY, wxT("Remise à zéro")); // bouton remise à zéro
 
 
 	wxString choices[117]; // Liste des années
@@ -49,20 +48,20 @@ Frame_IMG::Frame_IMG(): wxFrame(NULL, wxID_ANY, wxT("Calculateur d'IMG"), wxDefa
 		choices[i] = wxString::Format(wxT("%i"), 1900+i); // ajout des dates (1900, 1901, etc..) dans la liste
 	}
 
-	wxTextCtrl *input_prenom = new wxTextCtrl(fenetre, wxID_ANY); // input du prénom
-	wxSpinCtrl *input_poids  = new wxSpinCtrl(fenetre, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 1, 150, 50); // input du poid : peut aller de 1 à 150 kg
-	wxSpinCtrlDouble *input_taille = new wxSpinCtrlDouble(fenetre, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0.10, 2.50, 1.85, 0.01); // input de la taille : peut ale
+	input_prenom = new wxTextCtrl(fenetre, wxID_ANY); // input du prénom
+	input_poids  = new wxSpinCtrl(fenetre, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 1, 150, 50); // input du poid : peut aller de 1 à 150 kg
+	input_taille = new wxSpinCtrlDouble(fenetre, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0.10, 2.50, 1.85, 0.01); // input de la taille : peut ale
 
 
-	wxChoice *input_age = new wxChoice(fenetre, wxID_ANY, wxDefaultPosition, wxDefaultSize, 117, choices); // Création de la liste déroulante "Année" ( à partir de la variable choice remplie ci dessus)
+	input_age = new wxChoice(fenetre, wxID_ANY, wxDefaultPosition, wxDefaultSize, 117, choices); // Création de la liste déroulante "Année" ( à partir de la variable choice remplie ci dessus)
 	input_age->SetSelection(0); // Met la sélection de la liste sur le mot "Année"
 
-	wxRadioButton *input_garcon = new wxRadioButton(fenetre, wxID_ANY, wxT("Un garçon"), wxDefaultPosition, wxDefaultSize, wxRB_GROUP);
-	wxRadioButton *input_fille  = new wxRadioButton(fenetre, wxID_ANY, wxT("Une fille")); // Création des 2 input pour le sexe
+	input_garcon = new wxRadioButton(fenetre, wxID_ANY, wxT("Un garçon"), wxDefaultPosition, wxDefaultSize, wxRB_GROUP);
+	input_fille  = new wxRadioButton(fenetre, wxID_ANY, wxT("Une fille")); // Création des 2 input pour le sexe
 
-	wxCheckBox *input_frites  = new wxCheckBox(fenetre, wxID_ANY, wxT("Des frites")); // création des 3 checkbox
-	wxCheckBox *input_fruits  = new wxCheckBox(fenetre, wxID_ANY, wxT("Des fruits")); // à propos des goûts de l'utilisateur
-	wxCheckBox *input_legumes = new wxCheckBox(fenetre, wxID_ANY, wxT("Des légumes"));
+	input_frites  = new wxCheckBox(fenetre, wxID_ANY, wxT("Des frites")); // création des 3 checkbox
+	input_fruits  = new wxCheckBox(fenetre, wxID_ANY, wxT("Des fruits")); // à propos des goûts de l'utilisateur
+	input_legumes = new wxCheckBox(fenetre, wxID_ANY, wxT("Des légumes"));
 
 
 
@@ -122,9 +121,97 @@ Frame_IMG::Frame_IMG(): wxFrame(NULL, wxID_ANY, wxT("Calculateur d'IMG"), wxDefa
 	this->SetMinSize(wxSize(500, 220));
 
 	input_prenom->SetFocus();
+
+
+	input_prenom->Bind(wxEVT_TEXT,        &Frame_IMG::onCalculIMG, this);
+	input_poids->Bind( wxEVT_TEXT,        &Frame_IMG::onCalculIMG, this);
+	input_taille->Bind(wxEVT_TEXT,        &Frame_IMG::onCalculIMG, this);
+	input_age->Bind(   wxEVT_CHOICE,      &Frame_IMG::onCalculIMG, this);
+	input_garcon->Bind(wxEVT_RADIOBUTTON, &Frame_IMG::onCalculIMG, this);
+	input_fille->Bind( wxEVT_RADIOBUTTON, &Frame_IMG::onCalculIMG, this);
+
+	input_frites->Bind( wxEVT_CHECKBOX, &Frame_IMG::onCalculGouts, this);
+	input_fruits->Bind( wxEVT_CHECKBOX, &Frame_IMG::onCalculGouts, this);
+	input_legumes->Bind(wxEVT_CHECKBOX, &Frame_IMG::onCalculGouts, this);
+
+	bouton_raz->Bind(wxEVT_BUTTON, &Frame_IMG::onRAZ, this);
+
+	this->Bind(wxEVT_CLOSE_WINDOW, &Frame_IMG::onQuit, this);
 }
 
 Frame_IMG::~Frame_IMG()
 {
 
+}
+
+
+void Frame_IMG::onCalculIMG(wxCommandEvent &evenement)
+{
+	double img = 0.0;
+	wxDateTime now = wxDateTime::Today();
+
+
+	if(input_poids->GetValue() > 0 // Poids : on vérfie si l'input n'est pas vide
+	 &&
+	(input_garcon->GetValue() || input_fille->GetValue()) // On vérifie que "Garcon" ou "Fille" a été séléctionné
+	 &&
+	input_age->GetSelection() != wxNOT_FOUND && input_age->GetSelection() != 0 // Age : on vérifie que la séléction n'est pas sur "Année"
+	 &&
+	input_taille->GetValue() > 0) // Taille : On vérifie si l'input n'est pas vide
+	{
+		double age = now.GetYear()-(1900+input_age->GetSelection());
+		double taille = input_taille->GetValue();
+		double poids = input_poids->GetValue();
+		double sexe = 1.0;
+
+		if (input_fille->GetValue()) sexe = 0.0; // Si c'est une fille : la variable sexe vaut 0.
+
+
+		img = 1.2 * poids / (taille * taille)  + 0.23 * age - 10.8 * sexe - 5.4; // Calcul de l'IMG
+	}
+
+	input_resultat_IMG->SetValue(wxString::Format("%.2f",img)+" %");
+}
+
+void Frame_IMG::onCalculGouts(wxCommandEvent &evenement)
+{
+	if(input_frites->IsChecked() && !(input_legumes->IsChecked() || input_fruits->IsChecked()) )
+	{
+		texte_resultat_sain->SetLabel(wxT("Votre régime alimentaire est à risque.\nEssayez de manger 5 fruits et légumes par jour !"));
+	}
+	else
+	{
+		texte_resultat_sain->SetLabel(wxT("Vous avez une alimentation saine pour la santé."));
+	}
+
+
+}
+
+void Frame_IMG::onRAZ(wxCommandEvent &evenement)
+{
+	input_poids->SetValue(50);
+	input_taille->SetValue(1.85);
+	input_age->SetSelection(0);
+	input_garcon->SetValue(true);
+
+	input_frites->SetValue(false);
+	input_fruits->SetValue(false);
+	input_legumes->SetValue(false);
+
+	input_prenom->SetValue(wxT(""));
+}
+
+void Frame_IMG::onQuit(wxCloseEvent &evenement)
+{
+	if(evenement.CanVeto()) // Si on à le droit de véto
+	{
+		int quit = wxMessageBox(wxT("Voulez vous vraiment quitter ?"), wxT("Quitter..."), wxOK|wxCANCEL);
+
+		if(quit == wxCANCEL)
+		{
+			evenement.Veto(); // Bloque la fermeture du programme
+			return;
+		}
+	}
+	this->Destroy(); // Lance le processus de destruction de la fenêtre
 }
--- src/IMG.h
+++ src/IMG.h
@@ -1,11 +1,27 @@
+#include <wx/spinctrl.h>
+
 #ifndef HEADERS_INCLUS_IMG
 	#define HEADERS_INCLUS_IMG
 
 	class Frame_IMG : public wxFrame
 	{
+		private:
+			wxTextCtrl *input_resultat_IMG, *input_prenom;
+			wxStaticText *texte_resultat_sain;
+			wxButton *bouton_raz;
+			wxSpinCtrl *input_poids;
+			wxSpinCtrlDouble *input_taille;
+			wxChoice *input_age;
+			wxRadioButton *input_garcon, *input_fille;
+			wxCheckBox *input_frites, *input_fruits, *input_legumes;
+
 		public:
 			Frame_IMG();
 			~Frame_IMG();
+			void onCalculIMG(wxCommandEvent &evenement);
+			void onCalculGouts(wxCommandEvent &evenement);
+			void onRAZ(wxCommandEvent &evenement);
+			void onQuit(wxCloseEvent &evenement);
 	};
 
 #endif
